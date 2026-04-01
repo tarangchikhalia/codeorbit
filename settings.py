248073287330f460e7c615f9f3276636982f2ae9
Tarang Chikhalia <tarang.chikhalia@gmail.com>
2026-03-31 22:53:03 -0400

    feat: Add pydantic-settings package, setup configuration management using pydantic-settings.

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    
+    # secret keys
+    litellm_key: str
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
